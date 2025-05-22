f335285e4f855ba6eb94bc6c120ce51cdf2b0d46
Ramya Prakash <email.ramyasandy@gmail.com>
2025-05-22 10:53:14 -0700

    Added the swagger dependency to pom.xml Added a dummy RESTController for swagger

---
+++ src/main/java/com/kenyajug/regression/DummyRestController.java
@@ -0,0 +1,17 @@
+package com.kenyajug.regression;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dummy")
+public class DummyRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from DummyRestController!";
+
+    }
+}
+
